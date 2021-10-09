9299dbe0b44dd7efc6febb66f0a36fccafff8463
morrownr <morrownr@gmail.com>
2021-10-09 14:58:45 -0500

    changes based on testing monitor mode

--- start-mon.sh
+++ start-mon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_NAME="start-mon.sh"
-SCRIPT_VERSION="20211008"
+SCRIPT_VERSION="20211009"
 
 # Simple script to start monitor mode on the provided wlan interface
 
@@ -13,33 +13,36 @@ then
 	exit 1
 fi
 
-clear
 echo "Running ${SCRIPT_NAME} version ${SCRIPT_VERSION}"
 
 # Set color definitions (https://en.wikipedia.org/wiki/ANSI_escape_code)
-RED='\033[0;31m'
+RED='\033[1;31m'
 YELLOW='\033[0;33;1m'
-GREEN='\033[0;32m'
-CYAN='\033[0;36m'
+GREEN='\033[1;32m'
+CYAN='\033[1;36m'
 NC='\033[0m'
 
-iface0mon='wlan0mon'
+# Detect wlan interface name
+iface0=`iw dev | grep 'Interface' | sed 's/Interface //'`
 
-interface=${1:-wlan0}
+# iface0='wlan0mon'
+# iface0=${1:-wlan0}
 
-# Check if interface exists
-
-ip link set $interface down
+# Check if iface0 exists
+ip link set $iface0 down
 if [ $? -eq 0 ]
 then
-	ip link set $interface name $iface0mon
-	iw $iface0mon set monitor control
-	ip link set $iface0mon up
+# Do not rename the interface - this driver has a bug
+#	ip link set $interface name $iface0mon
+
+	iw $iface0 set monitor control
+	ip link set $iface0 up
+
 	iw dev
 else
 	echo -e "${RED}ERROR: ${YELLOW}Please provide an existing interface as parameter! ${NC}"
-	echo -e "${NC}Usage: start-mon.sh [interface:wlan0] ${NC}"
-	echo -e "${NC}Tips: check with ${CYAN}iw dev ${NC}which interfaces are available! ${NC}"
+	echo -e "${NC}Usage: ${CYAN}$ sudo ./start-mon.sh [interface:wlan0] ${NC}"
+	echo -e "${NC}Tip:   ${CYAN}$ iw dev ${NC}(displays available interfaces) ${NC}"
 	exit 1
 fi
 
