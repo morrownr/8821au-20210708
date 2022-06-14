fffe626bfcf03f972762e10ad064ef7344f013bf
morrownr <morrownr@gmail.com>
2022-06-14 15:31:01 -0500

    update ARM scripts

--- ARM_RPI.sh
+++ ARM_RPI.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 #
-# Purpose: Change settings in the Makefile to support compiling for Raspberry Pi OS (32 bit).
+# Purpose: Change settings in the Makefile to support compiling 32 bit
+# operating systems for Raspberry Pi Hardware.
 #
 # To make this file executable (if necessary):
 #
-# $ chmod +x raspiOS-32.sh
+# $ chmod +x ARM_RPI.sh
 #
 # To execute this file:
 #
-# $ ./raspiOS-32.sh
+# $ ./ARM_RPI.sh
 
 # getconf LONG_BIT (need to work on this)
 
 sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
-RESULT=$?
-
-if [[ "$RESULT" != "0" ]]; then
-	echo "An error occurred and I386 support was not turned off in Makefile."
-	exit 1
-else
-	echo "I386 support was turned off in Makefile as planned."
-fi
 
 sed -i 's/CONFIG_PLATFORM_ARM_RPI = n/CONFIG_PLATFORM_ARM_RPI = y/g' Makefile
 RESULT=$?
@@ -32,3 +25,5 @@ else
 	echo "Raspberry Pi OS (32 bit) support was turned on in Makefile as planned."
 	exit 0
 fi
+
+sed -i 's/CONFIG_PLATFORM_ARM64_RPI = y/CONFIG_PLATFORM_ARM64_RPI = n/g' Makefile
