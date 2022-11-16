eeb18d8df6729238f5680e329cd2b8f4f55d7f1a
morrownr <morrownr@gmail.com>
2022-11-15 22:54:33 -0600

    minor update

--- install-driver.sh
+++ install-driver.sh
@@ -83,7 +83,7 @@ then
 		echo "Please report this error."
 		echo "Please copy all screen output and paste it into the report."
 		echo "You will need to run the following before reattempting installation."
-		echo "$ sudo ./remove-driver-no-dkms.sh"
+		echo "$ sudo ./remove-driver.sh"
 		exit $RESULT
 	fi
 
@@ -102,7 +102,7 @@ then
 		echo "Please report this error."
 		echo "Please copy all screen output and paste it into the report."
 		echo "You will need to run the following before reattempting installation."
-		echo "$ sudo ./remove-driver-no-dkms.sh"
+		echo "$ sudo ./remove-driver.sh"
 		exit $RESULT
 	fi
 else
