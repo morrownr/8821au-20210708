morrownr/8821au-20210708

commit 7305e07787f5811ee1f5e39818edffa33dcccfb5

update scripts and documentation

install-driver.sh
@@ -28,19 +28,34 @@
 # GNU General Public License for more details.
 
 SCRIPT_NAME="install-driver.sh"
-SCRIPT_VERSION="20230227"
-MODULE_NAME="8821au"
+SCRIPT_VERSION="20231001"
+
+DRV_NAME="rt8821au"
 DRV_VERSION="5.12.5.2"
-
-KARCH="$(uname -m)"
-KVER="$(uname -r)"
-MODDESTDIR="/lib/modules/${KVER}/kernel/drivers/net/wireless/"
-
-DRV_NAME="rtl${MODULE_NAME}"
 DRV_DIR="$(pwd)"
+
+MODULE_NAME="8821au"
+
 OPTIONS_FILE="${MODULE_NAME}.conf"
 
-# check to ensure sudo was used to start the script
+#KARCH="$(uname -m)"
+if [ -z "${KARCH+1}" ]; then
+	KARCH="$(uname -m)"
+fi
+
+#KVER="$(uname -r)"
+if [ -z "${KVER+1}" ]; then
+	KVER="$(uname -r)"
+fi
+
+MODDESTDIR="/lib/modules/${KVER}/kernel/drivers/net/wireless/"
+
+#GARCH="$(uname -m | sed -e "s/i.86/i386/; s/ppc/powerpc/; s/armv.l/arm/; s/aarch64/arm64/; s/riscv.*/riscv/;")"
+if [ -z "${GARCH+1}" ]; then
+	GARCH="$(uname -m | sed -e "s/i.86/i386/; s/ppc/powerpc/; s/armv.l/arm/; s/aarch64/arm64/; s/riscv.*/riscv/;")"
+fi
+
+# check to ensure sudo or su - was used to start the script
 if [ "$(id -u)" -ne 0 ]; then
 	echo "You must run this script with superuser (root) privileges."
 	echo "Try: \"sudo ./${SCRIPT_NAME}\""
@@ -64,11 +79,6 @@ do
 	esac
 	shift
 done
-
-# ensure /usr/sbin is in the PATH so iw can be found
-if ! echo "$PATH" | grep -qw sbin; then
-        export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-fi
 
 # check to ensure gcc is installed
 if ! command -v gcc >/dev/null 2>&1; then
@@ -102,21 +112,26 @@ if [ ! -d "/lib/modules/$(uname -r)/build" ]; then
 	exit 1
 fi
 
+# ensure /usr/sbin is in the PATH so iw can be found
+#if ! echo "$PATH" | grep -qw sbin; then
+#        export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#fi
+
 # check to ensure iw is installed
-if ! command -v iw >/dev/null 2>&1; then
-	echo "A required package is not installed."
-	echo "Please install the following package: iw"
-	echo "Once the package is installed, please run \"sudo ./${SCRIPT_NAME}\""
-	exit 1
-fi
+#if ! command -v iw >/dev/null 2>&1; then
+#	echo "A required package is not installed."
+#	echo "Please install the following package: iw"
+#	echo "Once the package is installed, please run \"sudo ./${SCRIPT_NAME}\""
+#	exit 1
+#fi
 
 # check to ensure rfkill is installed
-if ! command -v rfkill >/dev/null 2>&1; then
-	echo "A required package is not installed."
-	echo "Please install the following package: rfkill"
-	echo "Once the package is installed, please run \"sudo ./${SCRIPT_NAME}\""
-	exit 1
-fi
+#if ! command -v rfkill >/dev/null 2>&1; then
+#	echo "A required package is not installed."
+#	echo "Please install the following package: rfkill"
+#	echo "Once the package is installed, please run \"sudo ./${SCRIPT_NAME}\""
+#	exit 1
+#fi
 
 DEFAULT_EDITOR="$(cat default-editor.txt)"
 # try to find the user's default text editor through the EDITORS_SEARCH array
@@ -136,10 +151,11 @@ echo ": ---------------------------"
 # displays script name and version
 echo ": ${SCRIPT_NAME} v${SCRIPT_VERSION}"
 
-# information that helps with bug reports
+# display kernel architecture
+echo ": ${KARCH} (kernel architecture)"
 
-# display architecture
-echo ": ${KARCH} (architecture)"
+# display architecture to send to gcc
+echo ": ${GARCH} (architecture to send to gcc)"
 
 SMEM=$(LANG=C free | awk '/Mem:/ { print $2 }')
 sproc=$(nproc)
@@ -148,6 +164,10 @@ if [ "$sproc" -gt 1 ]; then
 	if [ "$SMEM" -lt 1400000 ]
 	then
 		sproc=2
+	fi
+	if [ "$SMEM" -lt 700000 ]
+	then
+		sproc=1
 	fi
 fi
 
@@ -160,6 +180,10 @@ echo ": ${SMEM} (total system memory)"
 # display kernel version
 echo ": ${KVER} (kernel version)"
 
+# display version of gcc used to compile the kernel
+gcc_ver_used_to_compile_the_kernel=$(cat /proc/version | sed 's/^.*gcc/gcc/' | sed 's/\s.*$//')
+echo ": ""${gcc_ver_used_to_compile_the_kernel} (version of gcc used to compile the kernel)"
+
 # display gcc version
 gcc_ver=$(gcc --version | grep -i gcc)
 echo ": ""${gcc_ver}"
@@ -170,24 +194,25 @@ if command -v dkms >/dev/null 2>&1; then
 	echo ": ""${dkms_ver}"
 fi
 
-# display secure mode status if SecureBoot is enabled and if mokutil is installed
+# display Secure Boot status
 if command -v mokutil >/dev/null 2>&1; then
-	if mokutil --sb-state | grep -i  enabled >/dev/null 2>&1; then
-		echo ": SecureBoot enabled - read FAQ about SecureBoot"
-	fi
+	case $(mokutil --sb-state 2>&1) in
+		*enabled*) echo ": SecureBoot enabled" ;;
+		*disabled*) echo ": SecureBoot disabled" ;;
+		*) echo ": This system doesn't support Secure Boot" ;;
+	esac
+else
+	echo ": mokutil not installed"
 fi
-
 
 echo ": ---------------------------"
 echo
 
-echo "Checking for previously installed drivers."
+echo "Checking for previously installed drivers..."
 
 # check for and remove non-dkms installations
 # standard naming
 if [ -f "${MODDESTDIR}${MODULE_NAME}.ko" ]; then
-	echo ": ---------------------------"
-	echo
 	echo "Removing a non-dkms installation: ${MODDESTDIR}${MODULE_NAME}.ko"
 	rm -f "${MODDESTDIR}"${MODULE_NAME}.ko
 	/sbin/depmod -a "${KVER}"
@@ -202,8 +227,6 @@ fi
 # check for and remove non-dkms installations
 # with rtl added to module name (PClinuxOS)
 if [ -f "${MODDESTDIR}rtl${MODULE_NAME}.ko" ]; then
-	echo ": ---------------------------"
-	echo
 	echo "Removing a non-dkms installation: ${MODDESTDIR}rtl${MODULE_NAME}.ko"
 	rm -f "${MODDESTDIR}"rtl${MODULE_NAME}.ko
 	/sbin/depmod -a "${KVER}"
@@ -220,8 +243,6 @@ fi
 # Example: /usr/lib/modules/5.15.80-rockchip64/kernel/drivers/net/wireless/rtl8821cu/8821cu.ko.xz
 # Dear Armbiam, this is a really bad idea.
 if [ -f "/usr/lib/modules/${KVER}/kernel/drivers/net/wireless/${DRV_NAME}/${MODULE_NAME}.ko.xz" ]; then
-	echo ": ---------------------------"
-	echo
 	echo "Removing a non-dkms installation: /usr/lib/modules/${KVER}/kernel/drivers/net/wireless/${DRV_NAME}/${MODULE_NAME}.ko.xz"
 	rm -f /usr/lib/modules/"${KVER}"/kernel/drivers/net/wireless/${DRV_NAME}/${MODULE_NAME}.ko.xz
 	/sbin/depmod -a "${KVER}"
@@ -233,23 +254,26 @@ if [ -f "/usr/lib/modules/${KVER}/kernel/drivers/net/wireless/${DRV_NAME}/${MODU
 	echo "Removal complete."
 fi
 
-# check for and remove dkms installations
+# check for and remove all dkms installations with MODULE_NAME in DRV_NAME
+#
 if command -v dkms >/dev/null 2>&1; then
-	if dkms status | grep -i  ${DRV_NAME}; then
-		echo ": ---------------------------"
-		echo
-		echo "Removing a dkms installation."
-		dkms remove -m ${DRV_NAME} -v ${DRV_VERSION} --all
-		echo "Removing ${OPTIONS_FILE} from /etc/modprobe.d"
+	dkms status | while IFS="/, " read -r modname modver kerver _dummy; do
+		case "$modname" in *${MODULE_NAME})
+			echo "--> ${modname} ${modver} ${kerver}"
+			dkms remove -m "${modname}" -v "${modver}" -k "${kerver}" -c "/usr/src/${modname}-${modver}/dkms.conf"
+		esac
+	done
+	if [ -f /etc/modprobe.d/${OPTIONS_FILE} ]; then
 		rm -f /etc/modprobe.d/${OPTIONS_FILE}
-		echo "Removing source files from /usr/src/${DRV_NAME}-${DRV_VERSION}"
+	fi
+	if [ -f /usr/src/${DRV_NAME}-${DRV_VERSION} ]; then
 		rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
-		echo "Removal complete."
 	fi
 fi
 
-# sets module parameters (driver options) and blacklisted modules
+echo "Finished checking for and removing previously installed drivers."
 echo ": ---------------------------"
+
 echo
 echo "Starting installation."
 echo "Installing ${OPTIONS_FILE} to /etc/modprobe.d"
@@ -273,13 +297,21 @@ if ! command -v dkms >/dev/null 2>&1; then
 		exit $RESULT
 	fi
 
-# 	As shown in Makefile
-# 	install:
-#		install -p -m 644 $(MODULE_NAME).ko  $(MODDESTDIR)
-#		/sbin/depmod -a ${KVER}
-	make install
-	RESULT=$?
-
+#	if secure boot is active, use sign-install
+	if command -v mokutil >/dev/null 2>&1; then
+		if mokutil --sb-state | grep -i  enabled >/dev/null 2>&1; then
+			echo ": SecureBoot enabled - read FAQ about SecureBoot"
+			make sign-install
+			RESULT=$?
+		else
+			make install
+			RESULT=$?		
+		fi
+	else
+		make install
+		RESULT=$?
+	fi
+	
 	if [ "$RESULT" = "0" ]; then
         	make clean >/dev/null 2>&1
 		echo "The driver was installed successfully."
@@ -299,8 +331,10 @@ else
 # 	the dkms add command requires source in /usr/src/${DRV_NAME}-${DRV_VERSION}
 	echo "Copying source files to /usr/src/${DRV_NAME}-${DRV_VERSION}"
 	cp -rf "${DRV_DIR}" /usr/src/${DRV_NAME}-${DRV_VERSION}
+#	echo "${DRV_DIR}"
+#	echo "/usr/src/${DRV_NAME}-${DRV_VERSION}"
 
-	dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
+	dkms add -m ${DRV_NAME} -v ${DRV_VERSION} -k "${KVER}/${KARCH}" -c "/usr/src/${DRV_NAME}-${DRV_VERSION}/dkms.conf"
 	RESULT=$?
 
 #	RESULT will be 3 if the DKMS tree already contains the same module/version
@@ -322,12 +356,13 @@ else
 	else
 		echo "The driver was added to dkms successfully."
 		echo ": ---------------------------"
+		echo
 	fi
 
 	if command -v /usr/bin/time >/dev/null 2>&1; then
-		/usr/bin/time -f "Compile time: %U seconds" dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
+		/usr/bin/time -f "Compile time: %U seconds" dkms build -m ${DRV_NAME} -v ${DRV_VERSION} -k "${KVER}/${KARCH}" -c "/usr/src/${DRV_NAME}-${DRV_VERSION}/dkms.conf" --force
 	else
-		dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
+		dkms build -m ${DRV_NAME} -v ${DRV_VERSION} -k "${KVER}/${KARCH}" -c "/usr/src/${DRV_NAME}-${DRV_VERSION}/dkms.conf" --force
 	fi
 	RESULT=$?
 
@@ -343,7 +378,7 @@ else
 		echo ": ---------------------------"
 	fi
 
-	dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
+	dkms install -m ${DRV_NAME} -v ${DRV_VERSION} -k "${KVER}/${KARCH}" -c "/usr/src/${DRV_NAME}-${DRV_VERSION}/dkms.conf" --force
 	RESULT=$?
 
 	if [ "$RESULT" != "0" ]; then
@@ -361,13 +396,18 @@ else
 fi
 
 # provide driver upgrade information
-echo "Info: Upgrade this driver with the following commands as needed:"
+echo "Info: Update this driver with the following commands as needed:"
+echo
 echo "$ git pull"
 echo "$ sudo sh install-driver.sh"
-echo "Note: Upgrades to this driver should be performed before distro upgrades."
-echo "Note: Upgrades can be performed as often as you like."
-echo "Note: Work on this driver is continuous."
-echo ": ---------------------------"
+echo
+echo "Note: Updates to this driver SHOULD be performed before distro"
+echo "      upgrades such as Ubuntu 23.10 to 24.04."
+echo "Note: Updates can be performed as often as you like. It is"
+echo "      recommended to update at least every 2 months."
+echo "Note: Work on this driver, like the Linux kernel, is continuous."
+echo
+echo "Enjoy!"
 echo
 
 # unblock wifi

Kconfig
@@ -1,7 +1,6 @@
 config RTL8821AU
 	tristate "Realtek 8821A USB WiFi"
 	depends on USB
-	select WIRELESS_EXT
 	help
 	  Help message of RTL8821AU
 

Makefile
@@ -32,6 +32,9 @@ EXTRA_CFLAGS += -DCONFIG_LED_ENABLE
 EXTRA_CFLAGS += -Wno-address
 EXTRA_CFLAGS += -Wframe-larger-than=1648
 
+# gcc-13
+EXTRA_CFLAGS += -Wno-enum-int-mismatch
+
 GCC_VER_49 := $(shell echo `$(CC) -dumpversion | cut -f1-2 -d.` \>= 4.9 | bc )
 ifeq ($(GCC_VER_49),1)
 EXTRA_CFLAGS += -Wno-date-time	# Fix compile error && warning on gcc 4.9 and later
@@ -1352,20 +1355,8 @@ ifeq ($(CONFIG_PLATFORM_AUTODETECT), y)
 EXTRA_CFLAGS += -DCONFIG_LITTLE_ENDIAN
 EXTRA_CFLAGS += -DCONFIG_IOCTL_CFG80211 -DRTW_USE_CFG80211_STA_EVENT
 
-SUBARCH := $(shell uname -m)
-
-ifeq ($(SUBARCH), aarch64)
-SUBARCH := arm64
-endif
-
-ifeq ($(SUBARCH), armv7l)
-SUBARCH := arm
-endif
-
-ifeq ($(SUBARCH), armv6l)
-SUBARCH := arm
-endif
-
+#SUBARCH := $(shell uname -m)
+SUBARCH := $(shell uname -m | sed -e "s/i.86/i386/; s/ppc/powerpc/; s/armv.l/arm/; s/aarch64/arm64/; s/riscv.*/riscv/;")
 ARCH ?= $(SUBARCH)
 
 CROSS_COMPILE ?=
@@ -2516,7 +2507,8 @@ sign:
 	@mokutil --import MOK.der
 	@$(KSRC)/scripts/sign-file sha256 MOK.priv MOK.der 8821au.ko
 
-sign-install: all sign install
+sign-install:
+	sign install
 
 backup_rtlwifi:
 	@echo "Making backup rtlwifi drivers"
@@ -2573,4 +2565,5 @@ clean:
 	rm -fr Module.symvers ; rm -fr Module.markers ; rm -fr modules.order
 	rm -fr *.mod.c *.mod *.o .*.cmd *.ko *~
 	rm -fr .tmp_versions
+	rm -fr MOK.der MOK.priv
 endif

dkms-make.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Warning: Do not run this script in a terminal. It is designed to be
+#          run from another script.
+
 # SMEM needs to be set here if dkms build is not initiated by install-driver.sh
 SMEM=$(LANG=C free | awk '/Mem:/ { print $2 }')
 
@@ -10,6 +13,9 @@ sproc=$(nproc)
 if [ "$sproc" -gt 1 ]; then
 	if [ "$SMEM" -lt 1400000 ]; then
 		sproc=2
+	fi
+	if [ "$SMEM" -lt 700000 ]; then
+		sproc=1
 	fi
 fi
 

dkms.conf
@@ -4,4 +4,4 @@ BUILT_MODULE_NAME[0]="8821au"
 MAKE="./dkms-make.sh"
 CLEAN="'make' clean"
 DEST_MODULE_LOCATION[0]="/updates/dkms"
-AUTOINSTALL="YES"
+AUTOINSTALL="yes"

edit-options.sh
@@ -24,7 +24,7 @@
 # GNU General Public License for more details.
 
 SCRIPT_NAME="edit-options.sh"
-# SCRIPT_VERSION="20230126"
+# SCRIPT_VERSION="20230710"
 OPTIONS_FILE="8821au.conf"
 
 # check to ensure sudo was used to start the script
@@ -47,10 +47,16 @@ if ! command -v "${TEXT_EDITOR}" >/dev/null 2>&1; then
         exit 1
 fi
 
-${TEXT_EDITOR} /etc/modprobe.d/${OPTIONS_FILE}
+if [ -f "/etc/modprobe.d/${OPTIONS_FILE}" ]; then
+	${TEXT_EDITOR} /etc/modprobe.d/${OPTIONS_FILE}
+else
+	cp -f ${OPTIONS_FILE} /etc/modprobe.d
+	${TEXT_EDITOR} /etc/modprobe.d/${OPTIONS_FILE}
+fi
 
-printf "Do you want to apply the new options by rebooting now? (recommended) [y/N] "
-read -r REPLY
-case "$REPLY" in
-	[yY]*) reboot ;;
+printf "Do you want to apply the new options by rebooting now? (recommended) [Y/n] "
+read -r yn
+case "$yn" in
+	[nN]) ;;
+	*) reboot ;;
 esac

remove-driver.sh
@@ -28,18 +28,26 @@
 # GNU General Public License for more details.
 
 SCRIPT_NAME="remove-driver.sh"
-SCRIPT_VERSION="20230226"
-MODULE_NAME="8821au"
+SCRIPT_VERSION="20230830"
+
+DRV_NAME="rtl8821au"
 DRV_VERSION="5.12.5.2"
+MODULE_NAME="8821au"
 
-KARCH="$(uname -m)"
-KVER="$(uname -r)"
+#KARCH="$(uname -m)"
+if [ -z "${KARCH+1}" ]; then
+	KARCH="$(uname -m)"
+fi
+
+#KVER="$(uname -r)"
+if [ -z "${KVER+1}" ]; then
+	KVER="$(uname -r)"
+fi
+
 MODDESTDIR="/lib/modules/${KVER}/kernel/drivers/net/wireless/"
-
-DRV_NAME="rtl${MODULE_NAME}"
 OPTIONS_FILE="${MODULE_NAME}.conf"
 
-# check to ensure sudo was used to start the script
+# check to ensure sudo or su - was used to start the script
 if [ "$(id -u)" -ne 0 ]; then
 	echo "You must run this script with superuser (root) privileges."
 	echo "Try: \"sudo ./${SCRIPT_NAME}\""
@@ -49,7 +57,8 @@ fi
 # support for the NoPrompt option allows non-interactive use of this script
 NO_PROMPT=0
 # get the script options
-while [ $# -gt 0 ]; do
+while [ $# -gt 0 ]
+do
 	case $1 in
 		NoPrompt)
 			NO_PROMPT=1 ;;
@@ -70,8 +79,8 @@ echo ": ${SCRIPT_NAME} v${SCRIPT_VERSION}"
 
 # information that helps with bug reports
 
-# display architecture
-echo ": ${KARCH} (architecture)"
+# display kernel architecture
+echo ": ${KARCH} (kernel architecture)"
 
 # display kernel version
 echo ": ${KVER} (kernel version)"
@@ -105,17 +114,20 @@ if [ -f "/usr/lib/modules/${KVER}/kernel/drivers/net/wireless/${DRV_NAME}/${MODU
 	/sbin/depmod -a "${KVER}"
 fi
 
-# determine if dkms is installed and run the appropriate routines
+# check for and remove all dkms installations with DRV_NAME
+#
 if command -v dkms >/dev/null 2>&1; then
-	echo "Removing a dkms installation."
-	#  2>/dev/null suppresses the output of dkms
-	dkms remove -m ${DRV_NAME} -v ${DRV_VERSION} --all 2>/dev/null
+	dkms status | while IFS="/, " read -r modname modver kerver _dummy; do
+		case "$modname" in *${MODULE_NAME})
+			echo "--> ${modname} ${modver} ${kerver}"
+			dkms remove -m "${modname}" -v "${modver}" -k "${kerver}" -c "/usr/src/${modname}-${modver}/dkms.conf"
+		esac
+	done
 	RESULT=$?
-	#echo "Result=${RESULT}"
 
-	# RESULT will be 3 if there are no instances of module to remove
-	# however we still need to remove various files or the install script
-	# may complain.
+#	 RESULT will be 3 if there are no instances of module to remove
+#	 however we still need to remove various files or the install script
+#	 may complain.
 	if [ "$RESULT" = "0" ] || [ "$RESULT" = "3" ]; then
 		if [ "$RESULT" = "0" ]; then
 			echo "${DRV_NAME}/${DRV_VERSION} has been removed"

save-log.sh
@@ -1,37 +1,0 @@
-#!/bin/sh
-
-# Purpose: Save a log file with RTW lines only.
-#
-# To make this file executable:
-#
-# $ chmod +x save-log.sh
-#
-# To execute this file:
-#
-# $ sudo ./save-log.sh
-#
-# or
-#
-# $ sudo sh save-log.sh
-
-SCRIPT_NAME="save-log.sh"
-
-if [ "$(id -u)" -ne 0 ]; then
-	echo "You must run this script with superuser (root) privileges."
-	echo "Try: \"sudo ./${SCRIPT_NAME}\""
-	exit 1
-fi
-
-# deletes existing log
-rm -f -- rtw.log
-
-dmesg | cut -d"]" -f2- | grep "RTW" >> rtw.log
-RESULT=$?
-
-if [ "$RESULT" != "0" ]; then
-	echo "An error occurred while running: ${SCRIPT_NAME}"
-	echo "Did you set a log level > 0 ?"
-	exit 1
-else
-	echo "rtw.log saved successfully."
-fi

commit 379348549fe2c7d2ae408f740cdb812b41d73a06

Fix typo in DRV_NAME introduced in 7305e07

install-driver.sh
@@ -30,7 +30,7 @@
 SCRIPT_NAME="install-driver.sh"
 SCRIPT_VERSION="20231001"
 
-DRV_NAME="rt8821au"
+DRV_NAME="rtl8821au"
 DRV_VERSION="5.12.5.2"
 DRV_DIR="$(pwd)"
 